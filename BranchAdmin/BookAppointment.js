a418bf4407ff7843ecca1dc8e8d4560f4e0958f4
Fetena <117965359+Fetena@users.noreply.github.com>
2026-08-07 09:01:17 +0300

    Update BookAppointment.js

--- BranchAdmin/BookAppointment.js
+++ BranchAdmin/BookAppointment.js
@@ -2,7 +2,7 @@
 // ADMIN BOOKAPPOINTMENT (CHILD COMPONENT) - FIXED
 // ============================================
 
-class AdminBookings {
+class AdminBookAppointment {
     constructor(adminId) {
         this.adminId = adminId;
         this.appointments = [];
